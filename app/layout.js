ad23193476357add630246265902d16876e1f32a
JustinBenito <justinbenito1974@gmail.com>
2025-04-14 23:43:07 +0530

    here

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css"
 import { AuthProvider } from "@/context/auth-context"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,9 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        
         <ThemeProvider attribute="class" defaultTheme="light">
           <AuthProvider>
             {children}
+            <Analytics />
             <Toaster />
           </AuthProvider>
         </ThemeProvider>
